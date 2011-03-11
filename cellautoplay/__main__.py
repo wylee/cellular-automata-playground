0c32e1e31af6ed94d46e63e6e0fb66e5d8ff886d
Wyatt Lee Baldwin <wyatt.lee.baldwin@gmail.com>
2011-03-10 20:20:08 -0800

    Initial commit.

---
+++ cellautoplay/__main__.py
@@ -0,0 +1,42 @@
+import argparse
+import pickle
+import sys
+
+from cellautoplay import rules
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--rule', '-R', required=True)
+    parser.add_argument('--rows', '-r', type=int, default=21)
+    parser.add_argument('--cols', '-c', type=int, default=78)
+    parser.add_argument('--generations', '-g', type=int, default=20*(2**30))
+    parser.add_argument('--sleep-time', '-t', type=float, default=.1)
+    parser.add_argument('kwargs', nargs='*')
+    args = parser.parse_args()
+
+    RuleType = getattr(rules, args.rule)
+
+    if args.kwargs:
+        kwargs = dict(item.split('=') for item in args.kwargs)
+    else:
+        kwargs = {}
+
+    rule = RuleType(
+        (args.rows, args.cols), args.generations, args.sleep_time, **kwargs)
+
+    print_grid = rule.print_grid
+
+    # Save grid in case it produces super cool results
+    with open('cellular_automaton_grid.pickle', 'wb') as fp:
+        pickle.dump(rule.initial_grid, fp)
+
+    for generation in rule:
+        print_grid(generation)
+
+    print '\n' * args.rows
+    print 'Game over.'
+    return 1
+
+
+sys.exit(main())
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='cellautoplay',
+    description='Console-based script for playing with cellular automata.',
+    author='Wyatt L Baldwin <wyatt.lee.baldwin@gmail.com>',
+    version='0.1dev',
+    install_requires=(
+        'numpy',
+    ),
+)
